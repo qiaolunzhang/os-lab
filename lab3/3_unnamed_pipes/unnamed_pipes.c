ac68e44b49c7c2ff376a7c77b682da2104089cc9
qiaolunzhang <qiaolunzhang@gmail.com>
2018-06-16 13:00:04 +0800

    lab3-3 finished

--- lab3/3_unnamed_pipes/unnamed_pipes.c
+++ lab3/3_unnamed_pipes/unnamed_pipes.c
@@ -5,39 +5,51 @@
 
 int main()
 {
-int _pipe[2];
-int ret=pipe(_pipe);
-if(ret<0) {
-    perror("pipe\n");
-}
+    FILE *fp_read, *fp_write;
+    int pipe_fd[2];
+    int ret=pipe(pipe_fd);
 
-pid_t id=fork();
-if(id<0) {
-    perror("fork\n");
-} 
-else if(id==0)  {
-    // child
-    close(_pipe[0]);
-    int i=0;
-    char *mesg=NULL;
-    while(i<100) {
-        mesg="I am child";
-        write(_pipe[1],mesg,strlen(mesg)+1);
-        sleep(1);
-        ++i;
+    fp_read = fopen("./parent.txt", "r");
+    fp_write = fopen("./child.txt", "w+");
+    if (fp_read == NULL) {
+        printf("Cannot open file parent.txt");
     }
-}
-else  {
-    close(_pipe[1]);
-    int j=0;
-    char _mesg[100];
-    while(j<100) {
-        memset(_mesg,'\0',sizeof(_mesg ));
-        read(_pipe[0],_mesg,sizeof(_mesg));
-        printf("%s\n",_mesg);
-        j++;
+    if (fp_write == NULL) {
+        printf("Cannot open file child.txt");
     }
-}
+    if(ret<0) {
+        perror("pipe\n");
+    }
 
-return 0;
+    pid_t id=fork();
+    if(id<0) {
+        perror("fork\n");
+    } 
+    else if(id==0)  {
+        // child
+        close(pipe_fd[1]);
+        char mesg_receive[100];
+        memset(mesg_receive,'\0',sizeof(mesg_receive));
+        read(pipe_fd[0],mesg_receive,sizeof(mesg_receive));
+        fwrite(mesg_receive, 1, sizeof(mesg_receive), fp_write);
+        fclose(fp_write);
+    }
+    else  {
+        // 父进程，关闭读端
+        close(pipe_fd[0]);
+        char mesg_send[100];
+        int  count_read;
+        memset(mesg_send, '\0', sizeof(mesg_send));
+
+        count_read = fread(mesg_send, sizeof(char), 1024, fp_read);
+        while (count_read > 0) {
+            write(pipe_fd[1], mesg_send, strlen(mesg_send));
+            //printf("%s", mesg_send);
+            count_read = fread(mesg_send, sizeof(char), 1024, fp_read);
+            sleep(1);
+        }
+        fclose(fp_read);
+    }
+
+    return 0;
 }
