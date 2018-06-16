e31684e638ab5f850d3873856fd2abf883da535a
qiaolunzhang <qiaolunzhang@gmail.com>
2018-06-16 13:43:15 +0800

    lab3 -4 finished

--- lab3/4_named_pipes/client.c
+++ lab3/4_named_pipes/client.c
@@ -10,20 +10,26 @@
 
 int main()
 {
+    FILE * fp_write;
     int fd = open(PATH_NAME, O_RDONLY);
     if (fd<0) {
         printf("open file error");
     }
+    fp_write = fopen("./client.txt", "w+");
     char buf[BUF_SIZE];
     memset(buf, '\0', sizeof(buf));
-    while (1) {
-        int ret = read(fd, buf, sizeof(buf));
-        if (ret<0) {
-            printf("read end or error\n");
-            break;
-        }
-        printf("%s", buf);
+    int ret = read(fd, buf, sizeof(buf));
+    if (ret<0) {
+        printf("read end or error\n");
     }
+    printf("%s", buf);
+    int len = 0;
+    for (len = 0; len < BUF_SIZE; len++) {
+        if (buf[len] == '\0')
+            break;
+    }
+    fwrite(buf, sizeof(char), len, fp_write);
+    fclose(fp_write);
     close(fd);
     return 0;
 }
--- lab3/4_named_pipes/server.c
+++ lab3/4_named_pipes/server.c
@@ -9,23 +9,31 @@
 
 int main()
 {
+    FILE *fp_read;
+    int count_read;
     int ret = mkfifo(PATH_NAME, S_IFIFO|0666);
     if (ret==-1) {
         printf("make fifo error\n");
         return 1;
     }
 
+    fp_read = fopen("./server.txt", "r");
+    if (fp_read == NULL) {
+        printf("Cannot open file server.txt");
+    }
+
     char buf[BUF_SIZE];
     memset(buf, '\0', sizeof(buf));
 
     int fd=open(PATH_NAME, O_WRONLY);
-    while (1) {
-        fgets(buf, sizeof(buf)-1, stdin);
-        int ret = write(fd, buf, strlen(buf)+1);
+    count_read = fread(buf, sizeof(char), 1024, fp_read);
+    while (count_read > 0) {
+        int ret = write(fd, buf, strlen(buf));
         if (ret<0) {
             printf("write error");
             break;
         }
+        count_read = fread(buf, sizeof(char), 1024, fp_read);
     }
     close(fd);
     return 0;
